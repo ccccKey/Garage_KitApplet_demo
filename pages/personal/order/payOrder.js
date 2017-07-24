2aa27142ad8bfc30eb40878706f0cf986ece51bd
ccccKey <670897678@qq.com>
2017-07-24 13:15:47 +0800

    2017-7-24

--- pages/personal/order/payOrder.js
+++ pages/personal/order/payOrder.js
@@ -1,17 +1,43 @@
 // payOrder.js
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    shoppingCarData: [],
+    costPrice : 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var data = [];
+    for (var i = 0; i < app.shoppingCarData.length; i++) {
+      data.push(app.shoppingCarData[i]);
+    }
+
+    this.setData({
+      shoppingCarData: data
+    })
+
+    this.handleCost();
   },
+
+  //处理费用
+  handleCost: function (event) {
+    var shoppingCarData = this.data.shoppingCarData;
+    var cost = 0;
+
+    for (var i = 0; i < shoppingCarData.length; i++) {
+      cost += shoppingCarData[i].nums * shoppingCarData[i].shopData.costNum;
+    }
+
+    this.setData({
+      costPrice: cost
+    })
+  }
 })
